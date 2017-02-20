a32afb74098ed1607b9e57511bf0d21f1ef18df4
Renato Herebia <rherebia@gmail.com>
2017-02-20 10:20:24 -0400

    Assistindo aula 2 parte 2

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,5 +1,7 @@
 import {Component} from "@angular/core";
 import {Http} from '@angular/http';
+import {FotoService} from "../foto/foto.service";
+import {FotoComponent} from "../foto/foto.component";
 
 @Component({
     moduleId: module.id,
@@ -8,16 +10,13 @@ import {Http} from '@angular/http';
 })
 export class ListagemComponent {
 
-    fotos: Object[] = [];
+    fotos: FotoComponent[] = [];
 
-    constructor(http: Http) {
+    constructor(service: FotoService, http: Http) {
 
-        http.get('v1/fotos')
-            .map(res => res.json())
-            .subscribe(fotos => {
-                this.fotos = fotos;
-
-                console.log(this.fotos);
-            }, erro => console.log(erro));
+        service.lista().subscribe(fotos => {
+            this.fotos = fotos;
+            console.log(this.fotos);
+        }, erro => console.log(erro));
     }
 }
